3b17e14cfaa3b48e48f7c425c179c35de0b6f2e8
bbelgace <rina.sab.ben@gmail.com>
2021-11-02 17:07:00 +0100

    state

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import img from "./image.jpg";
+import Person from "./Person";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  state = {
+    isVisible: false,
+  };
+  toggleVisibility = () => {
+    this.setState({ isVisible: !this.state.isVisible });
+  };
+  state = {
+    Person: {
+      fullNAME: "sabrine ben belgacem ",
+      bio: "obtenu un diplome supérieur en développemnt systèms informatiques de ISET Gafsa",
+      profession: "Developpeuse",
+    },
+    show: false,
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <h1> first state</h1>
+        <button onClick={this.toggleVisibility} className="visibility-btn">
+          {this.state.isVisible ? "Hide Person" : "Show Person"}
+        </button>
+        {this.state.isVisible && (
+          <Person Person={this.state.Person}>
+            <img src={img} alt="Moi" />
+          </Person>
+        )}
+      </div>
+    );
+  }
 }
 
 export default App;
